1bf3ed56fd653cea030c3e445bb8810281589a9e
TheDreamingTree145 <dandersonsoftware@outlook.com>
2018-08-16 20:35:24 -0400

    refacted ping qbot

--- lib/time_check_bot.rb
+++ lib/time_check_bot.rb
@@ -12,24 +12,27 @@ class SlackClient
   end
 
 #data = im.list
-def ping_qbot
-  client.chat_postMessage(channel: 'DC2AK85HD', text: 'who is on?', as_user: true)
-  sleep(30.seconds)
-  data = client.im_history(channel: 'DC2AK85HD')
-  message = data.messages.first.text.scan(/^(.*)/).slice(2, 20)
-  users = message.collect do |el|
-    el = el[0].split(' | ')
-    if el && el[3].to_i >= 25
-      name = el[0].split(': ')[0].strip
-      user_data = client.users_search(user: name).members[0]
+  def ping_qbot
+    client.chat_postMessage(channel: 'DC2AK85HD', text: 'who is on?', as_user: true)
+    sleep(30.seconds)
+    data = client.im_history(channel: 'DC2AK85HD')
+    message = data.messages.first.text.scan(/^(.*)/).slice(2, 20)
+    users = get_users(message)
+    users.each do |user|
+      self.ping_member(user) unless user.nil?
     end
-    user_data
   end
-  users.each do |user|
-    self.ping_member(user) unless user.nil?
-  end
-end
 
+  def get_users(message)
+    message.collect do |el|
+      el = el[0].split(' | ')
+      if el && el[3].to_i >= 25
+        name = el[0].split(': ')[0].strip
+        user_data = client.users_search(user: name).members[0]
+        user_data
+      end
+    end.compact
+  end
 
   def ping_member(user_data)
     res = client.im_open(user: user_data.id)
